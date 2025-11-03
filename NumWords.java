24450182d6657641de8e2230225acba20574e953
Liron Moraz <liron.moraz@post.runi.ac.il>
2025-11-03 20:14:53 +0200

    fix2

--- NumWords.java
+++ NumWords.java
@@ -3,7 +3,7 @@ public class NumWords {
 	public static void main(String args[]) {
 	    int num = Integer.parseInt(args[0]);
 
-		int hundreds = (num / 100) % 10;
+		int hundreds = num / 100;
 		int tens = (num / 10) % 10;
 		int ones = num % 10;
 
